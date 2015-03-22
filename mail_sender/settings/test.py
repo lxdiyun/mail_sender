df224712fcbd186b5ed2f6016a7f3e6fc7e5537f
Adrian Lee <lxdiyun@gmail.com>
2015-03-22 21:55:25 +0800

    second commit

---
+++ mail_sender/settings/test.py
@@ -0,0 +1,21 @@
+"""
+This is an example settings/test.py file.
+Use this settings file when running tests.
+These settings overrides what's in settings/base.py
+"""
+
+from .base import *
+
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
+
+SECRET_KEY = 'mc^je*ujm3m*v6$jj6wod3t93ab3prr9-id&y1$eg_744=jvua'
